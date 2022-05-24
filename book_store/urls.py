cd692ed8cc013f7d2067785254981c3d041653da
uzquarantin <abdulrakhmon.mukhitdinov.97@gmail.com>
2022-05-24 18:01:16 +0500

    Test

--- book_store/urls.py
+++ book_store/urls.py
@@ -32,4 +32,5 @@ urlpatterns = [
     path('', include('books.urls')),
     path('docs/', include_docs_urls(title='Books API')),
     path('schema/', schema_view),
+    path('schema-v2/', schema_view),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
